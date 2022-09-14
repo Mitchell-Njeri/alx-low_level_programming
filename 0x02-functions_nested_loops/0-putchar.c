bdb78e0f664ae67d62947588da7ed8c02e9700cb
Njeri-Mitchell <mitchellnyam@gmail.com>
2022-09-14 07:47:39 -0700

    commit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,15 @@
-#include <stdio.h>
 #include "main.h"
 /**
-* _putchar - writes the character c to stdout
-* @c: The character to print
+* main - Entry point
 *
-* Return: On success 1.
-* On error, -1 is returned, and errno is set appropriately.
+* Return: On success 0
 */
-
 int main(void)
 {
 int count;
-int vals[] = {95,112,117,114,99,107,97,114};
+int vals[] = {95, 112, 117, 114, 99, 104, 97, 114};
 
-for(count = 0; count <= 7; count++)
+for (count = 0; count <= 7; count++)
 {
 _putchar(vals[count]);
 }
