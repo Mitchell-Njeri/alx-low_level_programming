48203d05dffdfa339fa34ee4df085ae0966d4846
Njeri-Mitchell <mitchellnyam@gmail.com>
2022-09-15 07:25:03 -0700

    4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
 * print_most_numbers - prints numbers from 0 - 9, except 2 and 4
 *
@@ -13,7 +12,7 @@ for (i = 48; i <= 57; i++)
 if (i == 50 || i == 52)
 continue;
 else
-putchar(i);
+_putchar(i);
 }
 _putchar('\n');
 }
